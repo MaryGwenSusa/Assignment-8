83c4379b5305fad21d300e77a0d46ad6c015e404
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-08 10:46:33 +0800

    Nested condition requirement to store the input to the initialized list

--- Lottery.py
+++ Lottery.py
@@ -15,3 +15,9 @@ while num <= 3: # limit for the input loop
     except ValueError: # input validation for decimals and alpa format
         print("\033[33mYou did not type an \033[4minteger.\033[00m")
         continue
+    if (0 <= guess <= 9):
+        if guess not in bets: # no duplicates
+            bets.append(guess)
+        else:
+            print("\033[31m\033[1mError!\033[00m \033[34mYou have \033[4malready picked this number\033[00m")
+            continue
