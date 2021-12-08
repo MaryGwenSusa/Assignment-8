8b369927c045d2a6daa6c1c25a8f30359a3813ba
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-08 10:44:08 +0800

    Loop for input plus validation

--- Lottery.py
+++ Lottery.py
@@ -9,3 +9,9 @@ def header():
 
 num = 1
 bets = [] # initialized an empty list
+while num <= 3: # limit for the input loop
+    try:
+        guess = int(input(f"Enter guess #" + str(num) + "(0-9): ")) 
+    except ValueError: # input validation for decimals and alpa format
+        print("\033[33mYou did not type an \033[4minteger.\033[00m")
+        continue
