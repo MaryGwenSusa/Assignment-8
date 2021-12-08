31ec6a61fa170a651d706acf921aeddfcd0e47e2
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-08 10:54:55 +0800

    Comments; defined function to produce lotto numbers with list function and random.sample function plus sorted function and of course, .append()

--- Lottery.py
+++ Lottery.py
@@ -8,21 +8,27 @@ def header():
 
 def userBet():
     num = 1
-    bets = []
-    while num <= 3:
+    bets = [] # initialized an empty list
+    while num <= 3: # limit for the input loop
         try:
             guess = int(input(f"Enter guess #" + str(num) + "(0-9): ")) 
-        except ValueError:
-            print("You did not type an integer.")
+        except ValueError: # input validation for decimals and alpa format
+            print("\033[33mYou did not type an \033[4minteger.\033[00m")
             continue
         if (0 <= guess <= 9):
-            if guess not in bets:
+            if guess not in bets: # no duplicates
                 bets.append(guess)
             else:
-                print("Error! You have already picked this number")
+                print("\033[31m\033[1mError!\033[00m \033[34mYou have \033[4malready picked this number\033[00m")
                 continue
         else:
-            print("Invalid input: out of range.")
+            print("\033[31m\033[1mInvalid input:\033[00m \033[35mout of range.\033[00m")
             continue
         num += 1
-    return sorted(bets) 
+    return sorted(bets)
+
+def winningNumbers():
+    lotteryNumbers = list() # variable for a list function
+    winningNum = sorted(random.sample(range(0,9), 3)) 
+    lotteryNumbers.append(winningNum) # adds items on the lotteryNumbers variable (for list)
+    return lotteryNumbers
