3ba97e508a8400e3483ed24b2691abb12c798c1c
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-09 13:51:40 +0800

    Defined a function for user's name; random number variable; and initialized true variable

--- Guess the Number.py
+++ Guess the Number.py
@@ -3,3 +3,9 @@ def header():
     title = "**GUESS THE NUMBER**"
     flower = "*" * len(title) 
     print('\033[93m{} \n{} \n{}\033[00m'.format(flower, title, flower)) # created a header design
+
+def guessingNumber():
+    UserName = input("\033[36mHello, what's your name?\033[00m \033[91m^_^\n>>>\033[00m ").title()
+    print('\033[92mOkay, '+ UserName + '. You are guessing a number between \033[1m0 and 100.\033[00m')
+    secretNum = anyFT(0,100) #random.choice
+    numberOfGuesses = True
